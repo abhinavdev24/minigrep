f59085d20b53e9f806633d6f60f0c71070235184
Abhinav Kumar <1rn18cs002.abhinav@gmail.com>
2022-03-30 18:02:13 +0530

    writing err msg to std err instead of std output

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+use minigrep;
 use std::env;
 use std::process;
-use minigrep;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing the arguments: {}", err);
+        eprintln!("Problem parsing the arguments: {}", err);
         process::exit(1);
     });
-    
+
     println!("Searching for : {}", config.query);
     println!("In file : {}\n", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
